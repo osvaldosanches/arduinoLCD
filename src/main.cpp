5b02fb6f92f48b53b4e27d32279ced1f2525001e
osvaldosanches <vado.benites@gmail.com>
2019-12-24 19:49:31 -0300

    Update main.cpp
    Arquivos para funcionamento do lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>  
+
+LiquidCrystal displayLCD(12, 11, 5, 4, 3, 2); //construtor (rs, enable e dados de comunicacao)
 
 void setup() {
-  // put your setup code here, to run once:
+
+  displayLCD.begin(16, 2);//número de colunas e linhas
+
+  displayLCD.print("Osvaldo Sanches");//texto
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  displayLCD.setCursor(0, 1);//coluna 0 e linha 1
+
+  displayLCD.print(millis() / 1000);//funcao que conta em mili
+
 }
+
